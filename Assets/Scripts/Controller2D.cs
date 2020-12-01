2c1ad80309e8b66b92009e9d22bd5863d2ca705c
Lucimakis <66547606+Lucimakis@users.noreply.github.com>
2020-11-30 23:45:20 -0800

    Movement(?)
    Added some colliders, movement for player, crouching, jumping, potential dash ability(?)

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool grounded;
+    private bool facingRight;
+    private BoxCollider2D boxCollider;
+    private float crouchSpeed;
+    private Vector3 m_velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        grounded = false;
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        facingRight = true;
+    }
+    void FixedUpdate()
+    {
+        Collider2D[] collisions = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+        for (int i = 0; i < collisions.Length; i++)
+        {
+            if (collisions[i] != boxCollider)
+            {
+                grounded = true;
+            }
+        }
+    }
+
+    public void Move(Vector2 velocity, bool crouch, bool dash, bool jump) 
+    {
+        if (crouch)
+        {
+            velocity = velocity / 2;
+        }
+        Vector3 target = new Vector2(velocity.x * 10f, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, target, ref m_velocity, 0f);
+        if ((velocity.x > 0 && !facingRight) || (velocity.x < 0 && facingRight)) 
+        {
+            Flip();
+        }
+        if (grounded && jump)
+        {
+            grounded = false;
+            rb.AddForce(new Vector2(0f, 1000f));
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.0f;
+    private Controller2D controller;
+    private Vector2 velocity;
+    private bool jump;
+    private bool crouch;
+
+    public void Awake()
+    {
+        controller = GetComponent<Controller2D>();
+    }
+
+    void FixedUpdate()
+    {
+        velocity = new Vector2(Input.GetAxis("Horizontal") + speed * Time.deltaTime, 0);
+        jump = Input.GetKey("up");
+        crouch = Input.GetKey("down");
+        controller.Move(velocity, crouch, false, jump);
+    }
+}

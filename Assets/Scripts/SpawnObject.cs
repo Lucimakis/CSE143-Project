81592347a54834f64654a8e64a41f33ee4880d17
Jebikoh <aneeshedara@gmail.com>
2020-12-05 12:21:15 -0500

    Add room prefabs & random tile spawn script

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    // Array of possible tiles
+    public GameObject[] objects;
+
+    private void Start()
+    {
+        // Pick and instantiate a random tile
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+}

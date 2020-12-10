6abe6627378e39533640ba8b59714f5271b0eabc
aaamwh <mwhao1204>
2020-12-10 16:23:41 +0800

    new room
    new room

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        GameObject instance = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
+    }
+}
